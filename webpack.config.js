b6b62de40fdfa7cc02c71acab2a853e005f33049
ezequiellivingston <pablo.livingston@leapsight.com>
2022-08-14 16:06:18 -0300

    config rules eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: "./src/main.js",
+  entry: './src/main.js',
 
-    devtool: "source-map",
+  devtool: 'source-map',
 
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
-            },
-            {
-                test: /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
-                type: "asset/resource",
-            },
-        ],
-    },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
+        type: 'asset/resource'
+      }
+    ]
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: "body",
-            template: "./public/index.html",
-            filename: path.resolve(__dirname, "./build/index.html"),
-        })
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './public/index.html',
+      filename: path.resolve(__dirname, './build/index.html')
+    })
+  ],
 
-    resolve: {
-        extensions: ["*", ".js", ".jsx"],
-        alias: {
-            "@": path.resolve(__dirname, "src/"),
-        },
-    },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src/')
+    }
+  },
 
-    output: {
-        path: path.resolve(__dirname, "build"),
-        clean: true
-    },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    clean: true
+  }
 }

b7f27c74611d7b192938f3cb3710e0482c2ec1b4
ezequiellivingston <pablo.livingston@leapsight.com>
2022-08-14 16:56:54 -0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,70 +4,83 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const StylelintPlugin = require('stylelint-webpack-plugin')
 
-module.exports = {
-  entry: './src/main.js',
+module.exports = (env, arg) => {
+  const { mode } = arg
 
-  devtool: 'source-map',
+  const isProduction = mode === 'production'
+  const filename = isProduction ? '[name].[contenthash]' : '[name]'
 
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.(s*)css$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          'css-loader',
-          'sass-loader'
-        ]
+  return {
+    entry: './src/main.js',
+
+    devtool: 'source-map',
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.(s*)css$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader },
+            'css-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: 'body',
+        template: './public/index.html',
+        filename: path.resolve(__dirname, './build/index.html'),
+        minify: isProduction
+      }),
+
+      new MiniCssExtractPlugin({
+        filename: `${filename}.css`
+      }),
+
+      new ESLintPlugin({
+        extensions: ['js', 'jsx', 'json'],
+        fix: false,
+        emitError: true,
+        emitWarning: true,
+        failOnError: true
+      }),
+
+      new StylelintPlugin({
+        extensions: ['scss', 'css'],
+        fix: false,
+        emitError: true,
+        emitWarning: true,
+        failOnError: true
+      })
+    ],
+
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+      alias: {
+        '@': path.resolve(__dirname, 'src/')
       }
-    ]
-  },
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      template: './public/index.html',
-      filename: path.resolve(__dirname, './build/index.html')
-    }),
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      clean: true,
+      filename: `${filename}.js`
+    },
 
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    }),
-
-    new ESLintPlugin({
-      extensions: ['js', 'jsx', 'json'],
-      fix: false,
-      emitError: true,
-      emitWarning: true,
-      failOnError: true
-    }),
-
-    new StylelintPlugin({
-      extensions: ['scss', 'css'],
-      fix: false,
-      emitError: true,
-      emitWarning: true,
-      failOnError: true
-    })
-  ],
-
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-    alias: {
-      '@': path.resolve(__dirname, 'src/')
+    optimization: {
+      minimize: isProduction
     }
-  },
-
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    clean: true
   }
 }

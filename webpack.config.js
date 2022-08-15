bfe84c7beed3c5c9dc984bcf38cc497642bc6aa0
ezequiellivingston <pablo.livingston@leapsight.com>
2022-08-15 14:20:27 -0300

    split webpack config: client and server

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,103 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const StylelintPlugin = require('stylelint-webpack-plugin')
 
-module.exports = (env, arg) => {
+const commonMdules = {
+  rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    },
+    {
+      test: /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
+      type: 'asset/resource'
+    },
+    {
+      test: /\.(s*)css$/,
+      use: [
+        { loader: MiniCssExtractPlugin.loader },
+        'css-loader',
+        'sass-loader'
+      ]
+    }
+  ]
+}
+
+const commonPlugins = (filename) => [
+  new ESLintPlugin({
+    extensions: ['js', 'jsx', 'json'],
+    fix: false,
+    emitError: true,
+    emitWarning: true,
+    failOnError: true
+  }),
+
+  new StylelintPlugin({
+    extensions: ['scss', 'css'],
+    fix: false,
+    emitError: true,
+    emitWarning: true,
+    failOnError: true
+  }),
+
+  new MiniCssExtractPlugin({
+    filename: `${filename}.css`
+  })
+]
+
+const commonResolve = {
+  extensions: ['*', '.js', '.jsx'],
+  alias: {
+    '@': path.resolve(__dirname, 'src/')
+  }
+}
+
+const configClient = (_, arg) => {
   const { mode } = arg
 
   const isProduction = mode === 'production'
   const filename = isProduction ? '[name].[contenthash]' : '[name]'
 
   return {
-    entry: {
-      server: './src/server.js',
-      main: './src/client.js'
+    entry: './src/client.js',
+
+    devtool: 'source-map',
+
+    module: commonMdules,
+
+    plugins: [
+      ...commonPlugins(filename),
+
+      new HtmlWebpackPlugin({
+        inject: 'body',
+        template: './public/index.html',
+        filename: path.resolve(__dirname, './build/public/index.html'),
+        excludeChunks: ['server'],
+        minify: isProduction
+      })
+    ],
+
+    resolve: commonResolve,
+
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: `${filename}.js`
     },
+
+    optimization: {
+      minimize: isProduction
+    }
+  }
+}
+
+const configServer = (_, arg) => {
+  const { mode } = arg
+
+  const isProduction = mode === 'production'
+  const filename = isProduction ? '[name].[contenthash]' : '[name]'
+
+  return {
+    entry: './src/server.js',
 
     devtool: 'source-map',
 
@@ -25,77 +111,21 @@ module.exports = (env, arg) => {
       __filename: false
     },
 
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        },
-        {
-          test: /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
-          type: 'asset/resource'
-        },
-        {
-          test: /\.(s*)css$/,
-          use: [
-            { loader: MiniCssExtractPlugin.loader },
-            'css-loader',
-            'sass-loader'
-          ]
-        }
-      ]
-    },
+    module: commonMdules,
 
-    plugins: [
-      new HtmlWebpackPlugin({
-        inject: 'body',
-        template: './public/index.html',
-        filename: path.resolve(__dirname, './build/public/index.html'),
-        excludeChunks: ['server'],
-        minify: isProduction
-      }),
+    plugins: commonPlugins(filename),
 
-      new MiniCssExtractPlugin({
-        filename: `${filename}.css`
-      }),
-
-      new ESLintPlugin({
-        extensions: ['js', 'jsx', 'json'],
-        fix: false,
-        emitError: true,
-        emitWarning: true,
-        failOnError: true
-      }),
-
-      new StylelintPlugin({
-        extensions: ['scss', 'css'],
-        fix: false,
-        emitError: true,
-        emitWarning: true,
-        failOnError: true
-      })
-    ],
-
-    resolve: {
-      extensions: ['*', '.js', '.jsx'],
-      alias: {
-        '@': path.resolve(__dirname, 'src/')
-      }
-    },
+    resolve: commonResolve,
 
     output: {
       path: path.resolve(__dirname, 'build'),
-      clean: true,
-      filename: ({ chunk }) => {
-        if (chunk.name === 'server') return '[name].js'
-
-        return `${filename}.js`
-      }
+      filename: 'server.js'
     },
 
     optimization: {
-      minimize: isProduction
+      minimize: false
     }
   }
 }
+
+module.exports = [configClient, configServer]

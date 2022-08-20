cab25a49270eca37eb94ae554e748ac8c05637d8
ezequiellivingston <pablo.livingston@leapsight.com>
2022-08-20 17:44:16 -0300

    Import entry style in component App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Routes, Route } from 'react-router-dom'
-import { routes } from './routes'
 
+import { routes } from './routes'
+import { Routes, Route } from 'react-router-dom'
+
+import Navbar from './layouts/Navbar'
 import './styles/main.scss'
 
 function App ({ initialProps }) {
   return (
-    <Routes>
-      {routes.map(({ path, component: Page }) => (<Route key={path} path={path} element={<Page {...initialProps} />} />))}
-    </Routes>
+    <>
+      <Navbar />
+
+      <Routes>
+        {routes.map(({ path, component: Page }) => (<Route exact key={path} path={path} element={<Page {...initialProps} />} />))}
+      </Routes>
+    </>
   )
 }
 

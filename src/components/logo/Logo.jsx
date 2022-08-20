9d25b080b6c08989d4d6955641ea818c029a7e88
ezequiellivingston <pablo.livingston@leapsight.com>
2022-08-20 17:48:16 -0300

    Add component Logo

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '@/assets/Logo_ML.png'
+
+function Logo () {
+  return (
+    <a href="/">
+      <img src={logo} height="32" />
+    </a>
+  )
+}
+
+export default Logo
